12e3f1a25893672d18d3e8de2ecac4d7b80b61a4
Ricardo <ricardo.costa@novageo.pt>
2020-05-12 15:44:02 +0100

    sprite sheet of the ships movement

--- Scripts/Comet.gd
+++ Scripts/Comet.gd
@@ -1,6 +1,7 @@
 extends RigidBody2D
 
-export var speed = 400
+export var min_speed = 160
+export var max_speed = 320
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
