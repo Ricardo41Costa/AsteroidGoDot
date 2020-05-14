4bc84b1fa9a808eee63198373383bf661d7bbee9
Ricardo <ricardo.costa@novageo.pt>
2020-05-14 13:04:09 +0100

    semi-working animation cycle

--- Scripts/SpaceShip.gd
+++ Scripts/SpaceShip.gd
@@ -10,8 +10,6 @@ func _ready():
 	animationState.start("Idle")
 	screen_size = get_viewport_rect().size
 	position = Vector2(screen_size.x / 2, screen_size.y / 2)
-	rotation = PI
-	rotation_degrees = 0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -19,17 +17,23 @@ func _physics_process(delta):
 	
 	var velocity = Vector2()
 	
-	if Input.is_action_pressed("ui_left"):
-		rotation_degrees -= speed * delta
-	
-	if Input.is_action_pressed("ui_right"):
-		rotation_degrees += speed * delta
-	
 	if Input.is_action_pressed("ui_up"):
-		animationState.travel("SpeedStable")
+		
+		if animationState.get_current_node() != "SpeedStable":
+			animationState.travel("SpeedStable")
+		
 		velocity = Vector2(0,-1).rotated(rotation) * speed * delta
+		
+		if Input.is_action_pressed("ui_left"):
+			rotation_degrees -= speed * delta
 	
-	velocity = move_and_collide(velocity)
+		if Input.is_action_pressed("ui_right"):
+			rotation_degrees += speed * delta
+	else:
+		if animationState.get_current_node() == "SpeedStable":
+			animationState.travel("Idle")
+	
+	move_and_collide(velocity)
 	
 	if position.y < -10:
 		position = Vector2(position.x, screen_size.y)
