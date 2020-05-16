96d5613fea91992762e024d3f73cbfa2ab546602
Ricardo <ricardo.costa@novageo.pt>
2020-05-16 19:08:32 +0100

    SpaceShip shooting is working :)

--- Scripts/SpaceShip.gd
+++ Scripts/SpaceShip.gd
@@ -2,16 +2,18 @@ extends KinematicBody2D
 
 export var speed = 250 #player speed
 export (PackedScene) var bulletScene
+export(NodePath) var parent_path
 
+var timeToWait = 0
 var animationState : AnimationNodeStateMachinePlayback #callback to the animation state machine
-var screen_size  # Size of the game window.
+var screenSize  # Size of the game window.
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	animationState = $AnimationTree.get("parameters/playback")
 	animationState.start("Idle")
-	screen_size = get_viewport_rect().size
-	position = Vector2(screen_size.x / 2, screen_size.y / 2)
+	screenSize = get_viewport_rect().size
+	position = Vector2(screenSize.x / 2, screenSize.y / 2)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
@@ -22,15 +24,21 @@ func _physics_process(delta):
 
 func CannonController():
 	
-	if Input.is_action_just_pressed("ui_accept"):
+	var currentTime = OS.get_ticks_msec()
+	
+	if Input.is_action_just_pressed("ui_accept" ) && currentTime >= timeToWait:
+		
+		timeToWait = currentTime + 400
 		
 		var leftBullet = bulletScene.instance()
 		leftBullet.global_position = $LeftCannonPosition.global_position
-		add_child(leftBullet)
+		leftBullet.rotation = rotation
+		get_node(parent_path).add_child(leftBullet)
 		
 		var rightBullet = bulletScene.instance()
 		rightBullet.global_position = $RightCannonPosition.global_position
-		add_child(rightBullet)
+		rightBullet.rotation = rotation
+		get_node(parent_path).add_child(rightBullet)
 
 # To Control the players input and move the space ship accordingly.
 func MovementController(delta):
@@ -59,13 +67,13 @@ func MovementController(delta):
 func BorderController():
 	
 	if position.y < -10:
-		position = Vector2(position.x, screen_size.y)
+		position = Vector2(position.x, screenSize.y)
 	
-	if position.y > (screen_size.y + 10):
+	if position.y > (screenSize.y + 10):
 		position = Vector2(position.x, 0)
 	
 	if position.x < -10:
-		position = Vector2(screen_size.x, position.y)
+		position = Vector2(screenSize.x, position.y)
 	
-	if position.x > (screen_size.x + 10):
+	if position.x > (screenSize.x + 10):
 		position = Vector2(0, position.y)
--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -1,21 +1,16 @@
 extends RigidBody2D
 
-export var speed = 100 #bullet speed
-var screen_size # screen size
+export var speed = 400 #bullet speed
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	screen_size = get_viewport().size # get screen size
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
 	
 	var velocity
 	
-	velocity = Vector2(0,-1).rotated(rotation) * speed * delta
-	position = velocity
-
+	velocity = Vector2(0,-1).rotated(global_rotation) * speed 
+	
+	apply_central_impulse(velocity)
 
 func _on_Visibility_screen_exited():
 	queue_free()
+	print("OUTSIDE OF SCREEN BYE BYE BITCH")
